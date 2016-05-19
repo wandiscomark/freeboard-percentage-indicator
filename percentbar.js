fda93fdd941fdd708f4886bc37940cbf7883f28a
mark.williamson <mark.williamson@wandisco.com>
2016-05-19 12:24:11 +0100

    cleanup

--- percentbar.js
+++ percentbar.js
@@ -2,42 +2,42 @@
 
     var percentBarWidget = function (settings) {
 
-       
+
         var self = this;
 
         var currentSettings = settings;
-		var displayElement = $('<div class="tw-display"></div>');
-		var titleElement = $('<h2 class="section-title tw-title tw-td"></h2>');
+        var displayElement = $('<div class="tw-display"></div>');
+        var titleElement = $('<h2 class="section-title tw-title tw-td"></h2>');
         var valueElement = $('<div class="tw-value"></div>');
         var unitsElement = $('<div class="tw-unit"></div>');
         var chartid = 'percentbarone';
         var currentValue = 0;
         var percentValue = 0;
 
-		function updateValueSizing()
-		{
-			if(!_.isUndefined(currentSettings.units) && currentSettings.units != "") // If we're displaying our units
-			{
-				valueElement.css("max-width", (displayElement.innerWidth() - unitsElement.outerWidth(true)) + "px");
-			}
-			else
-			{
-				valueElement.css("max-width", "100%");
-			}
-		}
+        function updateValueSizing()
+        {
+            if(!_.isUndefined(currentSettings.units) && currentSettings.units != "") // If we're displaying our units
+            {
+                valueElement.css("max-width", (displayElement.innerWidth() - unitsElement.outerWidth(true)) + "px");
+            }
+            else
+            {
+                valueElement.css("max-width", "100%");
+            }
+        }
 
         this.render = function (element) {
-			$(element).empty();
+            $(element).empty();
             $(element).append('<link rel="stylesheet" href="/plugins/percentbar/style.css" />');
 
             console.log(currentValue);
             console.log(percentValue);
 
             percent = Math.floor(Math.random(0,100)*100);
-            percent = percentValue; 
+            percent = percentValue;
             id='percentbarone' ;
             if(chartid.length) {
-                id=chartid; 
+                id=chartid;
             }
             var node = document.createElement("div");
             node.className = 'percentredbg';
@@ -48,77 +48,75 @@
             inner.style.width = '1%';
             inner.style.width = '0%';
             node.appendChild(inner);
-            //setTimeout('document.getElementById("'+id+'").style.width='+percent+'+"%"; ', 100);
-            //setTimeout('document.getElementById("'+id+'").style.background="#b3cd2c"; ', 100);
-            
-            
-			$(displayElement).append(titleElement).append(node);
+         
 
-			$(element).append(displayElement);
+            $(displayElement).append(titleElement).append(node);
 
-			updateValueSizing();
+            $(element).append(displayElement);
+
+            updateValueSizing();
         }
 
         this.onSettingsChanged = function (newSettings) {
             currentSettings = newSettings;
 
-			var shouldDisplayTitle = (!_.isUndefined(newSettings.title) && newSettings.title != "");
-			var shouldDisplayUnits = (!_.isUndefined(newSettings.units) && newSettings.units != "");
+            var shouldDisplayTitle = (!_.isUndefined(newSettings.title) && newSettings.title != "");
+            var shouldDisplayUnits = (!_.isUndefined(newSettings.units) && newSettings.units != "");
             if(newSettings.chartid){
                 chartid = newSettings.chartid;
             }
 
-			if(shouldDisplayTitle)
-			{
-				titleElement.html((_.isUndefined(newSettings.title) ? "" : newSettings.title));
-				titleElement.attr("style", null);
-			}
-			else
-			{
-				titleElement.empty();
-				titleElement.hide();
-			}
+            if(shouldDisplayTitle)
+            {
+                titleElement.html((_.isUndefined(newSettings.title) ? "" : newSettings.title));
+                titleElement.attr("style", null);
+            }
+            else
+            {
+                titleElement.empty();
+                titleElement.hide();
+            }
 
-			if(shouldDisplayUnits)
-			{
-				unitsElement.html((_.isUndefined(newSettings.units) ? "" : newSettings.units));
-				unitsElement.attr("style", null);
-			}
-			else
-			{
-				unitsElement.empty();
-				unitsElement.hide();
-			}
+            if(shouldDisplayUnits)
+            {
+                unitsElement.html((_.isUndefined(newSettings.units) ? "" : newSettings.units));
+                unitsElement.attr("style", null);
+            }
+            else
+            {
+                unitsElement.empty();
+                unitsElement.hide();
+            }
 
-			var valueFontSize = 30;
+            var valueFontSize = 30;
 
-			if(newSettings.size == "big")
-			{
-				valueFontSize = 75;
+            if(newSettings.size == "big")
+            {
+                valueFontSize = 75;
 
-			}
+            }
 
-			valueElement.css({"font-size" : valueFontSize + "px"});
+            valueElement.css({"font-size" : valueFontSize + "px"});
 
-			updateValueSizing();
+            updateValueSizing();
         }
 
-		this.onSizeChanged = function()
-		{
-			updateValueSizing();
-		}
+        this.onSizeChanged = function()
+        {
+            updateValueSizing();
+        }
 
-		this.updateBarWidth = function(id, value)
-		{
+        this.updateBarWidth = function(id, value)
+        {
             document.getElementById(id).style.width = 100 + "%";
             document.getElementById(id).innerHTML= value +"%";
             setTimeout('document.getElementById("'+id+'").style.width='+Math.floor(value)+'+"%"; ', 100);
-		}
-        
+        }
+
         this.onCalculatedValueChanged = function (settingName, newValue) {
-            
+
             this.updateBarWidth(settings.chartid, newValue)   ;
-           
+
         }
 
         this.onDispose = function () {
@@ -135,9 +133,9 @@
         }
 
         this.onSettingsChanged(settings);
-        
-        
-                function easeTransitionText(newValue, textElement, duration) {
+
+
+        function easeTransitionText(newValue, textElement, duration) {
 
             var currentValue = $(textElement).text();
 
@@ -173,8 +171,8 @@
                 $(textElement).text(newValue);
             }
         }
-        
- 
+
+
     };
 
     freeboard.loadWidgetPlugin({
@@ -190,28 +188,13 @@
                 type: "text"
             },
             {
-                name: "size",
-                display_name: "Size",
-                type: "option",
-                options: [
-                    {
-                        name: "Regular",
-                        value: "regular"
-                    },
-                    {
-                        name: "Big",
-                        value: "big"
-                    }
-                ]
-            },
-            {
                 name: "value",
                 display_name: "Value",
                 type: "calculated"
             },
             {
                 name: "chartid",
-                display_name: "Chart ID",
+                display_name: "Chart ID (unique, no spaces)",
                 type: "text"
             }
         ],
